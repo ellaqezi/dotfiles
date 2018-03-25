5233d5df5ecdae0a745b5a21ada548e68d520c97
ellaqezi <ellaqezi@gmail.com>
2018-03-25 18:30:15 +0200

    cleanup brew installations

--- brew/install.sh
+++ brew/install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 [[ -e '/usr/local/bin/brew' ]] && echo "Brew is installed" || yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>&1 
 brew analytics off
-
 exit 0

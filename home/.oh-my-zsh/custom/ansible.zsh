d8e314d0aa0b36d17bc7662d6dbce7cf6be23e63
ellaqezi <ellaqezi@gmail.com>
2018-04-19 08:27:10 +0200

    update custom prompt section ansible.zsh

--- home/.oh-my-zsh/custom/ansible.zsh
+++ home/.oh-my-zsh/custom/ansible.zsh
@@ -10,8 +10,9 @@
 # ------------------------------------------------------------------------------
 
 SPACESHIP_ANSIBLE_SHOW="${SPACESHIP_ANSIBLE_SHOW=true}"
-SPACESHIP_ANSIBLE_PREFIX="${SPACESHIP_ANSIBLE_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
-SPACESHIP_ANSIBLE_SUFFIX="${SPACESHIP_ANSIBLE_SUFFIX=" "}"
+SPACESHIP_ANSIBLE_SHOW_VERSION="${SPACESHIP_ANSIBLE_SHOW_VERSION=false}"
+SPACESHIP_ANSIBLE_PREFIX="${SPACESHIP_ANSIBLE_PREFIX="$SPACESHIP_ANSIBLE_PREFIX"}"
+SPACESHIP_ANSIBLE_SUFFIX="${SPACESHIP_ANSIBLE_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_ANSIBLE_SYMBOL="${SPACESHIP_ANSIBLE_SYMBOL="🅐"}"
 SPACESHIP_ANSIBLE_COLOR="${SPACESHIP_ANSIBLE_COLOR="white"}"
 
@@ -24,20 +25,21 @@ SPACESHIP_ANSIBLE_COLOR="${SPACESHIP_ANSIBLE_COLOR="white"}"
 # Otherwise this section won't be loaded.
 spaceship_ansible() {
   # If SPACESHIP_ANSIBLE_SHOW is false, don't show ansible section
-  [[ $SPACESHIP_ANSIBLE_SHOW == false ]] && return
+  [[ ${SPACESHIP_ANSIBLE_SHOW} == false ]] && return
+
+  # Check if ansible command is available for execution
+  spaceship::exists ansible || return
 
   # Show ansible section only when there are ansible-specific files in current
   # working directory.
   # Here glob qualifiers are used to check if files with specific extension are
   # present in directory. Read more about them here:
   # http://zsh.sourceforge.net/Doc/Release/Expansion.html
-  [[ -f ansible.cfg || -f .ansible.cfg || -n *.yml(#qN^/) ]] || return
-
-  # Check if ansible command is available for execution
-  spaceship::exists ansible || return
+  YAML=$(echo *.yml([1]N^/))
+  [[ -f ansible.cfg || -f .ansible.cfg || -f $YAML && $(ansible-playbook --syntax-check $YAML &> /dev/null) -eq 0 ]] || return
 
   # Retrieve ansible status and save it to variable
-  local ansible_status=$(ansible --version 2>&1 | head -n 1 | tr -d ansible)
+  [[ ${SPACESHIP_ANSIBLE_SHOW_VERSION} = true ]] && ansible_status=`ansible --version 2>&1 | head -n 1 | tr -d ansible`
 
   # Display ansible section
   spaceship::section \
